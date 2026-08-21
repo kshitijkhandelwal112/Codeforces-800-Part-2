4fb22cea35e3b6e05f24f6883e3af270b080725f
Your Name <kshitijkhandelwal112@gmail.com>
2026-08-21 16:21:43 +0530

    added AI best solution in last solution of mine

--- 2-509A.cpp
+++ 2-509A.cpp
@@ -13,3 +13,26 @@ int main() {
     cout<<grid[n-1][n-1]<<endl;
     return 0;
 }
+/* BEST SOLUTION GIVEN BY AI
+Whole grid not required, we can maintain just 1 row.
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n, 1);
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[j] += dp[j - 1];
+        }
+    }
+
+    cout << dp[n - 1] << endl;
+
+    return 0;
+}
+*/
